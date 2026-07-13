5d9a9d114afc11c395739f6f7b5296211c553daa
chaemin58 <sulv0309@naver.com>
2026-07-13 23:01:09 +0900

    fix: 루트 페이지 원래대로

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,3 @@
-import { NodiDetailHeader } from "@/components/nodi-detail/NodiDetailHeader";
-
 export default function Home() {
-  return (
-    <NodiDetailHeader
-      nodiTitle="고등학교 모임"
-      statusMessage="우리 언제만남?"
-      members={[
-        { name: "지훈" },
-        { name: "34" },
-        { name: "김성진" },
-        { name: "김성진" },
-        { name: "tlqkf" },
-        { name: "hi" },
-      ]}
-      startDate={new Date("2026-01-01")}
-      headCount={6}
-    />
-  );
+  return <div>nodi 입니다.</div>;
 }
